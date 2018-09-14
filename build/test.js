068ce5db8862a145f548a6b62a00f8a7f7501145
shaodahong <337097324@qq.com>
2018-09-14 15:39:05 +0800

    finish usage

---
+++ build/test.js
@@ -0,0 +1,38 @@
+const tinyPng = require('../lib')
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: path.join(__dirname, '../demo/index.js')
+  },
+  output: {
+    path: path.join(__dirname, '../test/')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              name: 'img/[name].[ext]',
+              publicPath: '/'
+            }
+          },
+          './lib/index.js'
+        ]
+      }
+    ]
+  },
+  optimization: {
+    minimize: false
+  }
+}
---
+++ demo/index.js
@@ -0,0 +1,4 @@
+const a = 1
+require('./imgs/1.jpg')
+require('./imgs/2.jpg')
+require('./imgs/3.jpg')
